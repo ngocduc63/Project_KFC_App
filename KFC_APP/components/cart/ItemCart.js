906114bd69de99161939b7d69f983836df0fd868
Nguyễn Ngọc Đức <duco060303@gmail.com>
2023-11-05 21:16:15 +0700

    Create list cart item

---
+++ KFC_APP/components/cart/ItemCart.js
@@ -0,0 +1,82 @@
+import { View, Text, Image, TouchableOpacity } from "react-native";
+import React, { useState } from "react";
+import { AntDesign } from "@expo/vector-icons";
+
+const ItemCart = (props) => {
+  const data = props.data;
+  const [isAvtiveDescription, SetIsActiveDescription] = useState(false);
+  const [quantity, SetQuantity] = useState(data.quantity);
+  const toggleDescription = () => {
+    SetIsActiveDescription(!isAvtiveDescription);
+  };
+  return (
+    <View className="bg-white rounded  mb-5">
+      <View className="flex-row w-full border-b-[1px] border-gray-400 border-solid">
+        <View className="h-28 w-1/3 mx-3 ">
+          <Image
+            source={{ uri: data.image }}
+            style={{ width: "100%", height: "100%" }}
+            resizeMode="contain"
+          />
+        </View>
+        <View className="mt-3 w-[60%] relative">
+          <Text className="font-semibold text-base">{data.name}</Text>
+          <TouchableOpacity
+            className="items-end w-full pr-3 -mt-5"
+            onPress={toggleDescription}
+          >
+            {!isAvtiveDescription && (
+              <AntDesign name="down" size={18} color="black" />
+            )}
+            {isAvtiveDescription && (
+              <AntDesign name="up" size={18} color="black" />
+            )}
+          </TouchableOpacity>
+          {isAvtiveDescription && (
+            <View className="h-24 mt-3">
+              <Text>{data.description}</Text>
+            </View>
+          )}
+          <View className="absolute bottom-3 flex-row gap-x-3">
+            <TouchableOpacity>
+              <Text className="font-bold underline">Chỉnh Sửa</Text>
+            </TouchableOpacity>
+            <TouchableOpacity>
+              <Text className="font-bold underline">Xóa</Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+      </View>
+      <View className="py-3">
+        <View className="flex-row items-center gap-x-3 ml-1">
+          <TouchableOpacity
+            className="h-8 w-8 items-center justify-center border border-black border-solid rounded-full"
+            onPress={() => {
+              if (quantity - 1 >= 0) SetQuantity(quantity - 1);
+            }}
+          >
+            <AntDesign name="minus" size={20} color="black" />
+          </TouchableOpacity>
+          <Text className="text-xl">{quantity}</Text>
+          <TouchableOpacity
+            className="h-8 w-8 items-center justify-center border border-black border-solid rounded-full"
+            onPress={() => SetQuantity(quantity + 1)}
+          >
+            <AntDesign name="plus" size={20} color="black" />
+          </TouchableOpacity>
+        </View>
+        <View className="items-end mr-3 -mt-8">
+          <Text className="text-xl font-semibold">
+            {data.price.toLocaleString("vi-VN", {
+              style: "currency",
+              currency: "VND",
+              minimumFractionDigits: 0,
+            })}
+          </Text>
+        </View>
+      </View>
+    </View>
+  );
+};
+
+export default ItemCart;
--- KFC_APP/screens/cart/CartList.js
+++ KFC_APP/screens/cart/CartList.js
@@ -4,13 +4,47 @@ import {
   TouchableHighlight,
   Image,
   TouchableOpacity,
+  FlatList,
 } from "react-native";
 import React from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { MaterialIcons, Entypo } from "@expo/vector-icons";
 import { ScrollView } from "react-native";
+import ItemCart from "../../components/cart/ItemCart";
 
 const CartList = ({ navigation }) => {
+  const listCart = [
+    // {
+    //   id: 1,
+    //   image:
+    //     "https://static.kfcvietnam.com.vn/images/items/lg/dinner-1.jpg?v=3JQQkg",
+    //   name: "KFC Dinner 79K",
+    //   price: 158000,
+    //   quantity: 1,
+    //   description:
+    //     "Cơm gà (1 Phần) + Gà Giòn Cay + Gà Rán(1 miếng) + Gà Giòn Cay + Pepsi Lon",
+    // },
+    // {
+    //   id: 2,
+    //   image:
+    //     "https://static.kfcvietnam.com.vn/images/items/lg/dinner-1.jpg?v=3JQQkg",
+    //   name: "KFC Dinner 79K",
+    //   price: 158000,
+    //   quantity: 1,
+    //   description:
+    //     "Cơm gà (1 Phần) + Gà Giòn Cay + Gà Rán(1 miếng) + Gà Giòn Cay + Pepsi Lon",
+    // },
+    // {
+    //   id: 3,
+    //   image:
+    //     "https://static.kfcvietnam.com.vn/images/items/lg/dinner-1.jpg?v=3JQQkg",
+    //   name: "KFC Dinner 79K",
+    //   price: 158000,
+    //   quantity: 1,
+    //   description:
+    //     "Cơm gà (1 Phần) + Gà Giòn Cay + Gà Rán(1 miếng) + Gà Giòn Cay + Pepsi Lon",
+    // },
+  ];
   return (
     <SafeAreaView>
       {/* Header */}
@@ -24,9 +58,7 @@ const CartList = ({ navigation }) => {
             style={{ width: 130, height: "50%" }}
           />
         </TouchableHighlight>
-        <TouchableOpacity
-          onPress={() => navigation.navigate("Profile")}
-        >
+        <TouchableOpacity onPress={() => navigation.navigate("Profile")}>
           <MaterialIcons name="account-circle" size={50} color="black" />
         </TouchableOpacity>
       </View>
@@ -53,26 +85,74 @@ const CartList = ({ navigation }) => {
         </TouchableOpacity>
       </View>
       {/* Content */}
-      <ScrollView className="mt-3 mx-2">
-        <Text className="text-3xl font-bold">Giỏ Hàng Của Tôi</Text>
-        <View className="h-[400px] bg-white mx-5 rounded-lg mt-5 px-5">
-          <Text className="text-3xl font-bold pt-5">
-            Giỏ Hàng Của Bạn Đang Trống. Hãy Đặt món ngay!
-          </Text>
-          <TouchableOpacity className="mt-3 bg-red-600 w-2/3 justify-center items-center py-3 rounded-full"
-          onPress={() => navigation.navigate("Menu")
-
-          }>
-            <Text className="text-white font-semibold">Bắt Đầu Đặt Hàng</Text>
-          </TouchableOpacity>
-          <View className = "w-2/3 h-[50%] mt-5 ml-10">
-          <Image
-            source={require("../../assets/logo_cart.png")}
-            style={{ width: "100%", height: "100%" }}
-          />
+      {listCart.length <= 0 && (
+        <View className="mt-3 mx-2">
+          <Text className="text-3xl font-bold">Giỏ Hàng Của Tôi</Text>
+          <View className="h-[400px] bg-white mx-5 rounded-lg mt-5 px-5">
+            <Text className="text-3xl font-bold pt-5">
+              Giỏ Hàng Của Bạn Đang Trống. Hãy Đặt món ngay!
+            </Text>
+            <TouchableOpacity
+              className="mt-3 bg-red-600 w-2/3 justify-center items-center py-3 rounded-full"
+              onPress={() => navigation.navigate("Menu")}
+            >
+              <Text className="text-white font-semibold">Bắt Đầu Đặt Hàng</Text>
+            </TouchableOpacity>
+            <View className="w-2/3 h-[50%] mt-5 ml-10">
+              <Image
+                source={require("../../assets/logo_cart.png")}
+                style={{ width: "100%", height: "100%" }}
+              />
+            </View>
           </View>
         </View>
-      </ScrollView>
+      )}
+      {listCart.length > 0 && (
+        <View className="mx-3">
+          <Text className="text-3xl font-bold">Giỏ Hàng Của Tôi</Text>
+          <FlatList
+            data={listCart}
+            showsVerticalScrollIndicator={false}
+            keyExtractor={(item, index) => index.toString()}
+            renderItem={({ item, index }) => (
+              <View className="mx-3">
+                <ItemCart data={item} />
+                {index === listCart.length - 1 && (
+                  <View>
+                    <View className="h-[300px] w-full bg-white rounded p-5 relative">
+                      <Text className="text-3xl font-bold">
+                        {listCart.length} MÓN
+                      </Text>
+                      <View className="h-[1px] w-full bg-gray-400 mt-5"></View>
+                      <View className="absolute bottom-4 items-center w-full ml-4">
+                        <TouchableOpacity className="w-[90%] bg-red-600 py-3 rounded-full flex-row justify-around">
+                          <Text className="font-bold text-white text-xl">
+                            Thanh Toán
+                          </Text>
+                          <Text className="font-bold text-white text-xl">
+                            {listCart
+                              .reduce(
+                                (accumulator, currentItem) =>
+                                  accumulator + currentItem.price,
+                                0
+                              )
+                              .toLocaleString("vi-VN", {
+                                style: "currency",
+                                currency: "VND",
+                                minimumFractionDigits: 0,
+                              })}
+                          </Text>
+                        </TouchableOpacity>
+                      </View>
+                    </View>
+                    <View className="h-[600px] w-full"></View>
+                  </View>
+                )}
+              </View>
+            )}
+          />
+        </View>
+      )}
     </SafeAreaView>
   );
 };
